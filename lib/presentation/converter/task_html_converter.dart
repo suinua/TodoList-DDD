4810551f70efd73a752131cc6b525ab900c686d8
suinua <maatinosippo@gmail.com>
2020-07-01 18:10:45 +0900

    チェックボダンの追加

--- lib/presentation/converter/task_html_converter.dart
+++ lib/presentation/converter/task_html_converter.dart
@@ -6,15 +6,30 @@ class TaskHtmlConverter {
   static HtmlElement execute(TaskDTO dto) {
     var html = '''
 <div class="${TaskHtmlClassName.body}" id="${dto.id}">
+ <div class="${TaskHtmlClassName.checkButton}" id="${TaskHtmlClassName.checkButton + dto.id}">
+  <i class="far fa-square" id="${TaskHtmlClassName.checkBox + dto.id}"></i>
+  <i class="far fa-check-square"  id="${TaskHtmlClassName.checkedBox + dto.id}"></i>
+ </div>
  <p>${dto.text}</p>
- <i class="fas fa-minus-square"></i>
+ <div class="${TaskHtmlClassName.deleteButton}" id="${TaskHtmlClassName.deleteButton + dto.id}">
+  <i class="fas fa-minus-square"></i>
+ </div>
 </div>
 ''';
+    var htmlElement = Element.html(html);
+    htmlElement
+        .querySelector('#${TaskHtmlClassName.checkedBox + dto.id}')
+        .style
+        .display = 'none';
 
-    return Element.html(html);
+    return htmlElement;
   }
 }
 
 class TaskHtmlClassName {
   static final body = 'task';
+  static final checkButton = 'checkTaskButton';
+  static final checkBox = 'checkBox';
+  static final checkedBox = 'checkedBox';
+  static final deleteButton = 'deleteTaskButton';
 }
